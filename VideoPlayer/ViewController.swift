b8133d72ef0cb804dcd12ce568571430403b70c5
Barry <fm334142@gmail.com>
2019-05-07 13:26:42 +0800

    Delete storybaod, add swiftlint

---
+++ VideoPlayer/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by Chen Yi-Wei on 2019/5/7.
+//  Copyright © 2019 Chen Yi-Wei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
